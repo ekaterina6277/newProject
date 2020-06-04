025676bff6c0ba79062ef135d3434b6498abb190
ekaterina6277 <61350598+ekaterina6277@users.noreply.github.com>
2020-06-04 18:45:46 +0300

    аккордеон

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-function addItemDom(item) {
-    let template = `
-    <div class="faq" data-item-id="${item.id}">
-    
-    <div class="remove-item" data-id="${item.id}">
-    X
-    </div>
-    </div>`;
-    
-    let wrapperList = document.querySelector('.faq');
-    wrapperList.innerHTML = template + wrapperList.innerHTML;
+document.querySelectorAll('.drop-down__item').forEach(element => addEventAccordion(element))
+
+function addEventAccordion(itemAccordion) {
+    const buttonItemAccordion = itemAccordion.querySelector('.button');
+    buttonItemAccordion.addEventListener('click', () => {
+        let isOpened = buttonItemAccordion.classList.contains('line');
+
+        if (isOpened) {
+            buttonItemAccordion.classList.remove('line');
+            let info = itemAccordion.querySelector('.drop-down__item-buttom');
+            info.style.display = 'none'
+            debugger
+        } else {
+            buttonItemAccordion.classList.add('line');
+            let info = itemAccordion.querySelector('.drop-down__item-buttom');
+            info.style.display = 'block'
+        }
+    });
 }
